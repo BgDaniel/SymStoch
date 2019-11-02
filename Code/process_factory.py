a564a7a99c2a610b854ee2ea7b14827fa4deda36
Daniel Berger <danielberger@daniels-air.speedport.ip>
2019-11-02 13:05:10 +0100

    deactivate multiprocessing and minor bug fixes in process_factory.py

--- Code/process_factory.py
+++ Code/process_factory.py
@@ -3,8 +3,8 @@ import math
 import matplotlib.pyplot as plt
 import time
 import sys
-from generalutils.monitoring import TrackExecutionTime
-
+from generalutils.monitoring import *
+import multiprocessing
 
 class SimulationConfig:
     @property
@@ -38,13 +38,13 @@ class Mu:
     def __init__(self, entries):
         self._entries = entries
 
-    def __call__(self, s, dW):                
+    def __call__(self, s):                
         assert len(self._entries) == len(s), 'x is of wrong length!'
         l = len(s)
         ret = np.zeros(l)
 
         for i in range(0, l):
-            ret[i] = self._entries[i](s) * dW[i]
+            ret[i] = self._entries[i](s)
 
         return ret
 
@@ -53,15 +53,15 @@ class Cov:
     def __init__(self, cov):
         self._cov = cov
 
-    def __call__(self, s, dW):            
-        assert len(self._cov) == len(s), 'x is of wrong length!'
-        l = len(s)
+    def __call__(self, x, dW):            
+        assert len(self._cov) == len(x), 'x is of wrong length!'
+        l = len(x)
         ret = np.zeros(l)
 
         for i in range(0, l):
             v = 0
             for j in range(0, l):
-                v += self._cov[i][j](s) * dW[j]
+                v += self._cov[i][j](x) * dW[j]
             ret[i] = v
 
         return ret
@@ -95,7 +95,6 @@ def my_function(range):
         sum += i
     return sum
 
-
 class MultiDimensionItoProcess:
 
     @property
@@ -109,6 +108,7 @@ class MultiDimensionItoProcess:
         self._dim = len(mu.Entries)        
         self._dW_t = np.zeros((self._dim, self._simuConfig.NumberSimus, self._simuConfig.TimeSteps))
         self._S0 = S0
+        self._dt = .0
         self._nbSimus = self._simuConfig.NumberSimus
         self._nbSteps = self._simuConfig.TimeSteps
         self._parallel = parallel
@@ -116,29 +116,28 @@ class MultiDimensionItoProcess:
     
     @TrackExecutionTime
     def generatePaths(self):
-        if self._parallel:
-            if __name__ == "__main__":
-                paths = np.zeros((self._dim, self._simuConfig.NumberSimus, self._simuConfig.TimeSteps))
-                nb_cpu = multiprocessing.cpu_count()
-                calc_range = range(0, self._simuConfig.NumberSimus)
-                range_size = len(calc_range)
-                chunk_size = int(math.ceil(float(range_size) /float(nb_cpu)))
-                chunks = [calc_range[i:i + chunk_size - 1] for i in range(0, range_size, chunk_size)]
+        if self._parallel and __name__ == "__main__":
+            paths = np.zeros((self._dim, self._simuConfig.NumberSimus, self._simuConfig.TimeSteps))
+            nb_cpu = multiprocessing.cpu_count()
+            calc_range = range(0, self._simuConfig.NumberSimus)
+            range_size = len(calc_range)
+            chunk_size = int(math.ceil(float(range_size) /float(nb_cpu)))
+            chunks = [calc_range[i:i + chunk_size - 1] for i in range(0, range_size, chunk_size)]
 
-                pool = multiprocessing.Pool(processes=nb_cpu)
-                paths_chunks = pool.map(self._generatePaths, (chunk for chunk in chunks))
+            pool = multiprocessing.Pool(processes=nb_cpu)
+            paths_chunks = pool.map(self._generatePaths, (chunk for chunk in chunks))
                 
-                for i, paths_chunk in enumerate(paths_chunks):
-                    simus = chunks[i]
-                    for j, k in enumerate(simus):
-                        paths[:,k,:] = paths_chunk[:,j,:]
+            for i, paths_chunk in enumerate(paths_chunks):
+                simus = chunks[i]
+                for j, k in enumerate(simus):
+                    paths[:,k,:] = paths_chunk[:,j,:]
                 
-                return paths
+            return paths
         else:
             progressBar = ProgressBar(50, "Generating paths ...", self._nbSimus)
-            return self._generatePaths(range(0, self._nbSimus))
+            return self._generatePaths(range(0, self._nbSimus), progressBar)
 
-    def _generatePaths(self, range_simus):
+    def _generatePaths(self, range_simus, progressBar=None):
         ell = len(range_simus)
         paths = np.zeros((self._dim, ell, self._simuConfig.TimeSteps))
 
@@ -147,13 +146,16 @@ class MultiDimensionItoProcess:
 
         for simu in range(0, ell):
             for time in range(1, self._nbSteps):
-                paths[:,simu,time] = paths[:,simu,time-1] + self._cov(paths[:,simu,time-1], self._dW_t[:,range_simus[simu],time]) 
-                paths[:,simu,time] = paths[:,simu,time] + self._mu(paths[:,simu,time-1], self._dW_t[:,range_simus[simu],time])
+                paths[:,simu,time] = paths[:,simu,time-1] + self._cov(paths[:,simu,time-1], self._dW_t[:,range_simus[simu],time-1])
+                paths[:,simu,time] = paths[:,simu,time] + self._mu(paths[:,simu,time-1]) * self._dt
+
+            progressBar.updateAndShow(simu + 1)
 
         return paths
 
     def initialize(self):
         dt = self._simuConfig.Dt
+        self._dt = dt
         dt_sqrt = math.sqrt(dt)
        
         self._dW_t = np.random.normal(size =(self._dim, self._nbSimus, self._nbSteps)) * dt_sqrt
@@ -177,7 +179,6 @@ class MultiDimensionItoProcess:
                 transformedS[:,simu,time] = transformation(self._S[:,simu,time])
 
         return None
-
 
 
 
--- Code/process_h_plus.py
+++ Code/process_h_plus.py
@@ -4,7 +4,15 @@ from mathtools.math_utils import Const
 class ProcessHPlus(MultiDimensionItoProcess):
     def __init__(self, simuConfig, S_0, sigma_0, mu, sigma_S, sigma_sigma, parallel=True):
         
-        cov = Cov([[lambda s, sigma: sigma_S / sigma, lambda s, sigma: .0], [lambda s, sigma: .0, lambda s, sigma: sigma_S / sigma]])        
+        def _cov_ii(x):
+            assert len(x) == 2, "Wrong input dimension!"
+            return sigma_S * x[1]
+                    
+        def _cov_ij(x):
+            assert len(x) == 2, "Wrong input dimension!"
+            return .0
+
+        cov = Cov([[_cov_ii, _cov_ij], [_cov_ij, _cov_ii]])        
         MultiDimensionItoProcess.__init__(self, simuConfig, mu, cov, [S_0, sigma_0], parallel)
 
   
