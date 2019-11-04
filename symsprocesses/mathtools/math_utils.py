be8689a4d6a460b4b8a3b30cf51cf90827431306
BgDaniel <Daniel.Berger@l-p-a.com>
2019-11-04 22:26:10 +0100

    plot curvilinear coordinate system

--- symsprocesses/mathtools/math_utils.py
+++ symsprocesses/mathtools/math_utils.py
@@ -11,7 +11,53 @@ class Const:
     def __call__(x):
         return self._value
 
-class TwoDimLinearMesh:
+class CurvedMesh2D:
+    def __init__(self, X, Y, offset):
+        self._X = X
+        self._Y = Y
+        self._offset = offset
+
+    def plot(self):
+        x_min = + float('inf')
+        y_min = + float('inf')
+        x_max = - float('inf')
+        y_max = - float('inf')
+
+        for line_y in self._X:
+            for p in line_y:
+                if p[0] < x_min:
+                    x_min = p[0]
+                if p[0] > x_max:
+                    x_max = p[0]
+                if p[1] < y_min:
+                    y_min = p[1]
+                if p[1] > y_max:
+                    y_max = p[1]
+
+        for line_x in self._Y:
+            for p in line_x:
+                if p[0] < x_min:
+                    x_min = p[0]
+                if p[0] > x_max:
+                    x_max = p[0]
+                if p[1] < y_min:
+                    y_min = p[1]
+                if p[1] > y_max:
+                    y_max = p[1]
+
+        for x_in_y_dir in self._X:
+            x, y = [p[0] for p in x_in_y_dir], [p[1] for p in x_in_y_dir]
+            plt.plot(x, y, linewidth=.4, color='blue')
+        for y_in_x_dir in self._Y:
+            x, y = [p[0] for p in y_in_x_dir], [p[1] for p in y_in_x_dir]
+            plt.plot(x, y, linewidth=.4, color='blue')
+       
+        plt.xlim(x_min - self._offset, x_max + self._offset)
+        plt.ylim(y_min - self._offset, y_max + self._offset)
+
+        plt.show()
+
+class LinearMesh2D:
     def __init__(self, x, y, x0, x1, y0, y1, step_width_x, step_width_y, step_width_line_x, step_width_line_y,
         offset=0.25):
         assert x0 < x1, "x0 must be smaller than x1!"
@@ -58,24 +104,24 @@ class TwoDimLinearMesh:
         for x in self._X:
             x_in_y_dir = []
             for x_y in x:
-                x_in_y_dir.append(diffeo(x_y))
+                x_in_y_dir.append(diffeo(x_y).toVector())
             X.append(x_in_y_dir)
 
         for y in self._Y:
             y_in_x_dir = []
             for y_x in y:
-                y_in_x_dir.append(diffeo(y_x))
+                y_in_x_dir.append(diffeo(y_x).toVector())
             Y.append(y_in_x_dir)
 
-        return X, Y
+        return CurvedMesh2D(X, Y, self._offset)
 
     def plot(self):
         for x_in_y_dir in self._X:
             x, y = [p[0] for p in x_in_y_dir], [p[1] for p in x_in_y_dir]
-            plt.plot(x, y, linewidth=.6, color='blue')
+            plt.plot(x, y, linewidth=.4, color='blue')
         for y_in_x_dir in self._Y:
             x, y = [p[0] for p in y_in_x_dir], [p[1] for p in y_in_x_dir]
-            plt.plot(x, y, linewidth=.6, color='blue')
+            plt.plot(x, y, linewidth=.4, color='blue')
        
         plt.xlim(self._x0 - self._offset, self._x1 + self._offset)
         plt.ylim(self._y0 - self._offset, self._y1 + self._offset)
@@ -84,10 +130,8 @@ class TwoDimLinearMesh:
         plt.show()
   
 
-        
-
-
-linear_mesh = TwoDimLinearMesh(.0, 1.0, - .5, + .5, .5, 1.5, .025, .025, .005, .005)
+linear_mesh = LinearMesh2D(.0, 1.0, - .5, + .5, .5, 1.5, .025, .025, .005, .005)
 linear_mesh.create()
 #linear_mesh.plot()
-linear_mesh.map(reflection0(1.0))
+curved_mesh = linear_mesh.map(reflection0(1.0))
+curved_mesh.plot()
