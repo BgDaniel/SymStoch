cf6ffe32f74c5da06ae827732620daa3a5864a61
Berger, Daniel <Daniel.Berger@L-P-A.com>
2019-11-05 20:07:05 +0100

    small refactoring of process_h_plus: make covariance functions class members

--- symsprocesses/process_h_plus.py
+++ symsprocesses/process_h_plus.py
@@ -2,17 +2,26 @@ from symsprocesses.process_factory import MultiDimensionItoProcess, Cov
 from symsprocesses.mathtools.math_utils import Const
 
 class ProcessHPlus(MultiDimensionItoProcess):
-    def __init__(self, simuConfig, S_0, sigma_0, mu, sigma_S, sigma_sigma, parallel=True):
-        
-        def _cov_ii(x):
-            assert len(x) == 2, "Wrong input dimension!"
-            return sigma_S * x[1]
+    def _cov_11(self, x):
+        assert len(x) == 2, "Wrong input dimension!"
+        return self._sigma_S * x[1]
                     
-        def _cov_ij(x):
-            assert len(x) == 2, "Wrong input dimension!"
-            return .0
+    def _cov_22(self, x):
+        assert len(x) == 2, "Wrong input dimension!"
+        return self._sigma_sigma * x[1]
 
-        cov = Cov([[_cov_ii, _cov_ij], [_cov_ij, _cov_ii]])        
+    def _cov_12(self, x):
+        assert len(x) == 2, "Wrong input dimension!"
+        return .0
+
+    def _cov_21(self, x):
+        return self._cov_12(x)
+
+    def __init__(self, simuConfig, S_0, sigma_0, mu, sigma_S, sigma_sigma, parallel=True):
+        self._sigma_S = sigma_S
+        self._sigma_sigma = sigma_sigma
+
+        cov = Cov([[self._cov_11, self._cov_12], [self._cov_21, self._cov_22]])        
         MultiDimensionItoProcess.__init__(self, simuConfig, mu, cov, [S_0, sigma_0], parallel)
 
   
